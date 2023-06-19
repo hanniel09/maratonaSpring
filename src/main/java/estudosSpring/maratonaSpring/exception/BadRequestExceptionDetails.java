73ab0158ca710a7a61c1c617c703118c51ce1671
hanniel09 <hannielvieira1227@gmail.com>
2023-06-19 18:14:04 -0300

    Feature

---
+++ src/main/java/estudosSpring/maratonaSpring/exception/BadRequestExceptionDetails.java
@@ -0,0 +1,16 @@
+package estudosSpring.maratonaSpring.exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class BadRequestExceptionDetails {
+    private String title;
+    private int status;
+    private String details;
+    private String developerMessage;
+    private LocalDateTime timestamp;
+}
---
+++ src/main/java/estudosSpring/maratonaSpring/handler/RestExceptionHandler.java
@@ -0,0 +1,25 @@
+package estudosSpring.maratonaSpring.handler;
+
+import estudosSpring.maratonaSpring.exception.BadRequestException;
+import estudosSpring.maratonaSpring.exception.BadRequestExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre){
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Check the Documentation")
+                        .details(bre.getMessage())
+                        .developerMessage(bre.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+}
