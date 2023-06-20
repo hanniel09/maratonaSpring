50baf8b49e514fd4e56b9e631a230fe6a2c81ecc
hanniel09 <hannielvieira1227@gmail.com>
2023-06-20 13:27:35 -0300

    Feature

---
+++ src/main/java/estudosSpring/maratonaSpring/exception/ExceptionDetails.java
@@ -0,0 +1,17 @@
+package estudosSpring.maratonaSpring.exception;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Data
+@SuperBuilder
+public class ExceptionDetails{
+    protected String title;
+    protected int status;
+    protected String details;
+    protected String developerMessage;
+    protected LocalDateTime timestamp;
+}
---
+++ src/main/java/estudosSpring/maratonaSpring/exception/ValidationExceptionDetails.java
@@ -0,0 +1,11 @@
+package estudosSpring.maratonaSpring.exception;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class ValidationExceptionDetails extends ExceptionDetails{
+    private final String fields;
+    private final String fieldsMassege;
+}
