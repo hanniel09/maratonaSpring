20fda6b39f5d6dfff7cbd3969318760d3cb7f48e
hanniel09 <hannielvieira1227@gmail.com>
2023-06-20 15:00:23 -0300

    Feature

--- src/main/java/estudosSpring/maratonaSpring/client/SpringClient.java
+++ src/main/java/estudosSpring/maratonaSpring/client/SpringClient.java
@@ -2,8 +2,13 @@ package estudosSpring.maratonaSpring.client;
 
 import estudosSpring.maratonaSpring.domain.Anime;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
@@ -14,5 +19,12 @@ public class SpringClient {
 
         Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 8);
         log.info(object);
+
+        ResponseEntity<List<Anime>> animes = new RestTemplate().exchange("http://localhost:8080/animes/all", HttpMethod.GET, null,
+                new ParameterizedTypeReference<>() {
+                });
+        log.info(animes.getBody());
+
+
     }
 }
